ccbdbe109d5ba467b41fa0a8deb64353eb814d1d
joshuaRMS <jlatusia@hotmail.com>
2016-12-11 23:44:39 +0100

    Added Pictures and Created Some pages Designed the Login Screen

---
+++ FindYourBuddiesApp/Pages/LoginPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace FindYourBuddiesApp.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class BlankPage1 : Page
+    {
+        public BlankPage1()
+        {
+            this.InitializeComponent();
+        }
+
+
+        //TODO add Navigation change 
+        private void CreateAccountButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        //TODO add Login stuff
+        private void LoginButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
