a3c39bb7a21256c44e7049f80b9244310e035a98
joshuaRMS <jlatusia@hotmail.com>
2017-01-19 23:33:18 +0100

    Changed so that you cant add yourself or an already friend + fixed the Userfile error with Json serialisation

--- FindYourFriendsServer/UsersFile.cs
+++ FindYourFriendsServer/UsersFile.cs
@@ -10,57 +10,57 @@ namespace FindYourFriendsServer
     {
         public List<User> Users = new List<User>();
 
-        public List<User> GetUsers() => JsonConvert.DeserializeObject<UsersFile>(File.ReadAllText("users.json")).Users;
+        public List<User> GetUsers() => JsonConvert.DeserializeObject<List<User>>(File.ReadAllText("users.json"));
 
         public static User GetUser(string username)
-            => JsonConvert.DeserializeObject<UsersFile>(File.ReadAllText("users.json"))
-                .Users.First(a => a.UserName == username);
+            => JsonConvert.DeserializeObject<List<User>>(File.ReadAllText("users.json"))
+               .First(a => a.UserName == username);
 
-        public static bool UsernameTaken(string username)
+
+        public static List<User> GetUsersFile()
         {
-            var userList = JsonConvert.DeserializeObject<UsersFile>(File.ReadAllText("users.json")).Users;
-            foreach (var user in userList)
-                if (user.UserName == username)
-                    return false;
-
-            return true;
+            return JsonConvert.DeserializeObject<List<User>>(File.ReadAllText("users.json"));
         }
 
-        public static List<User> GetUsersContaingString(string username)
-        {       
 
-            var userList = JsonConvert.DeserializeObject<UsersFile>(File.ReadAllText("users.json")).Users;
+        // Gets Json with all users, checks if @param username exists in the userlist Json.
+        public static bool IsUsernameAvaialable(string username)
+        {
+            var userList = GetUsersFile();
+            // LINQ ---
+            return userList.All(user => user.UserName != username);
+            // LINQ ---
+        }
+
+        // Returns a list with all the users in users.json containing @param username
+        public static List<User> GetUsersContaingString(string username)
+        {
+
+            var userList = GetUsersFile();
             //LINQ ---
             return userList.Where(u => u.UserName.Contains(username)).ToList();
             //LINQ ---
      
         }
 
-        public static List<User> GetAllFriendsByID(List<int> ids)
+        // Returns a list with all the users who contain @param ids as their userID
+        public static List<User> GetAllFriendsById(List<int> ids)
         {
-            var userList = JsonConvert.DeserializeObject<UsersFile>(File.ReadAllText("users.json")).Users;
-            List<User> friends = new List<User>();
-            foreach (var user in userList)
-            {
-                if(ids.Contains(user.UserId))
-                {
-                    friends.Add(user);
-                }
-            }
-            return friends;
+            var userList = GetUsersFile();
+            return userList.Where(user => ids.Contains(user.UserId)).ToList();
         }
 
         
         public static void RemoveUser(string username)
         {
-            int i = 0;
-            var usersfile = JsonConvert.DeserializeObject<UsersFile>(File.ReadAllText("users.json"));
-            foreach (var user in usersfile.Users)
+            var i = 0;
+            var usersfile = GetUsersFile();
+            foreach (var user in usersfile)
             {
 
                 if (user.UserName == username)
                 {
-                    usersfile.Users.RemoveAt(i);
+                    usersfile.RemoveAt(i);
                     File.WriteAllText("users.json", JsonConvert.SerializeObject(usersfile));
                     return;
                 }
@@ -69,34 +69,39 @@ namespace FindYourFriendsServer
 
         }
 
-        //TODO test
+        // Adds a friend_id to an given user.
+        // Get the user with @param username.
+        //
         public static void AddFriendToUser(string username, string friend)
         {
             if (GetUser(username) != null)
             {
                 // get the user from the userfile and the friends id 
                 // Adds the friend id to the user and updates the user.
-                var usersfile = JsonConvert.DeserializeObject<UsersFile>(File.ReadAllText("users.json"));
                 var user  = GetUser(username);
                 var friendID = GetUser(friend).UserId;
                 //TODO check if friends isnt already in the list
 
+                // if the user already has the friend ID or its his own ID and then it doens't add anything
+                if (user.Friends.Contains(friendID) || user.UserId == friendID)
+                    return;
+
                 RemoveUser(username);
-                var updatedFile =  JsonConvert.DeserializeObject<UsersFile>(File.ReadAllText("users.json"));
+                var updatedFile =  JsonConvert.DeserializeObject<List<User>>(File.ReadAllText("users.json"));
 
                 if (user.Friends == null)
                 {
                     user.Friends = new List<int>();
                 }
                 user.Friends.Add(friendID);
-                updatedFile.Users.Add(user);
+                updatedFile.Add(user);
                 File.WriteAllText("users.json", JsonConvert.SerializeObject(updatedFile));
             }
         }
 
         public static bool ValidateAccount(string username, string password)
         {
-            var userList = JsonConvert.DeserializeObject<UsersFile>(File.ReadAllText("users.json")).Users;
+            var userList = JsonConvert.DeserializeObject<List<User>>(File.ReadAllText("users.json"));
             foreach (var user in userList)
             {
                 if (username == user.UserName && password == user.PasswordHash)
@@ -111,29 +116,29 @@ namespace FindYourFriendsServer
         public static void UpdateUser(User user)
         {
             RemoveUser(user.UserName);
-            var userfile = JsonConvert.DeserializeObject<UsersFile>(File.ReadAllText("users.json"));
-            userfile.Users.Add(user);
+            var userfile = JsonConvert.DeserializeObject<List<User>>(File.ReadAllText("users.json"));
+            userfile.Add(user);
             File.WriteAllText("users.json", JsonConvert.SerializeObject(userfile));
         }
 
         public static void AddNewUser(User user)
         {
-            var usersfile = JsonConvert.DeserializeObject<UsersFile>(File.ReadAllText("users.json"));
-            var nextID = usersfile.Users.Select(a => a.UserId).Max() + 1;
+            var usersfile = JsonConvert.DeserializeObject<List<User>>(File.ReadAllText("users.json"));
+            var nextID = usersfile.Select(a => a.UserId).Max() + 1;
             user.UserId = nextID;
 
-            usersfile.Users.Add(user);
+            usersfile.Add(user);
             File.WriteAllText("users.json", JsonConvert.SerializeObject(usersfile));
         }
 
         public static void AddNewUser(string firstname, string lastname, string username, string password, int age,
             bool man, List<int> friends  )
         {
-            var usersfile = JsonConvert.DeserializeObject<UsersFile>(File.ReadAllText("users.json"));
+            var usersfile = JsonConvert.DeserializeObject<List<User>>(File.ReadAllText("users.json"));
 
-            var nextID = usersfile.Users.Select(a => a.UserId).Max() + 1;
+            var nextID = usersfile.Select(a => a.UserId).Max() + 1;
 
-            usersfile.Users.Add(new User
+            usersfile.Add(new User
             {
                 Age = age,
                 FirstName = firstname,
