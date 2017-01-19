06dcdc8ce5eb6400914f7b3efe352df71156fb86
casdeGroot <casdegroot@live.nl>
2017-01-20 00:51:07 +0100

    Old TCP CLIENT

--- FindYourBuddiesApp/TcpClient.cs
+++ FindYourBuddiesApp/TcpClient.cs
@@ -10,8 +10,8 @@ namespace FindYourBuddiesApp
     class TcpClient
     {
         private Socket _socket;
-        private static string _ip = "127.0.0.1";
-        private static int _port = 1337;
+        private static string IP = "192.168.1.101";
+        private static int Port = 1337;
 
         public static void DoRequest(Packet p, Action<Packet> responseCallback)
         {
@@ -20,50 +20,51 @@ namespace FindYourBuddiesApp
 
         public void SendPacket(Packet p, Action<Packet> responseCallback)
         {
-            using (_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            SocketAsyncEventArgs connectArgs = new SocketAsyncEventArgs
             {
-                SocketAsyncEventArgs connectArgs = new SocketAsyncEventArgs
+                RemoteEndPoint = new IPEndPoint(IPAddress.Parse(IP), Port)
+            };
+
+            connectArgs.Completed += delegate
+            {
+                SocketAsyncEventArgs sendArgs = new SocketAsyncEventArgs();
+
+                var json = JsonConvert.SerializeObject(p);
+
+                var jsonbytes = Encoding.ASCII.GetBytes(json);
+
+                sendArgs.SetBuffer(jsonbytes, 0, jsonbytes.Length);
+
+                //sendArgs.RemoteEndPoint = _socket.RemoteEndPoint;
+
+                sendArgs.Completed += delegate
                 {
-                    RemoteEndPoint = new IPEndPoint(IPAddress.Parse(_ip), _port)
-                };
+                    var recvArgs = new SocketAsyncEventArgs { RemoteEndPoint = _socket.RemoteEndPoint };
 
-                connectArgs.Completed += delegate (object cs, SocketAsyncEventArgs a)
-                {
-                    SocketAsyncEventArgs sendArgs = new SocketAsyncEventArgs();
-
-                    var json = JsonConvert.SerializeObject(p);
-
-                    var jsonbytes = Encoding.ASCII.GetBytes(json);
-
-                    sendArgs.SetBuffer(jsonbytes, 0, jsonbytes.Length);
-
-                    //sendArgs.RemoteEndPoint = _socket.RemoteEndPoint;
-
-                    sendArgs.Completed += delegate (object ss, SocketAsyncEventArgs se)
+                    recvArgs.Completed += delegate (object s, SocketAsyncEventArgs e)
                     {
-                        var recvArgs = new SocketAsyncEventArgs {RemoteEndPoint = _socket.RemoteEndPoint};
-
-                        recvArgs.Completed += delegate(object rs, SocketAsyncEventArgs re)
-                        {
-                            var response = Encoding.UTF8.GetString(re.Buffer, re.Offset, re.BytesTransferred);
-                            responseCallback(JsonConvert.DeserializeObject<Packet>(response));
-                        };
-                        recvArgs.SetBuffer(new byte[68140], 0, 68140);
-
-                        ((Socket)ss).ReceiveAsync(recvArgs);
+                        var response = Encoding.UTF8.GetString(e.Buffer, e.Offset, e.BytesTransferred);
+                        Disconnect();
+                        responseCallback(JsonConvert.DeserializeObject<Packet>(response));
 
                     };
+                    recvArgs.SetBuffer(new byte[68140], 0, 68140);
 
-                    ((Socket)cs).SendAsync(sendArgs);
+                    _socket.ReceiveAsync(recvArgs);
+
                 };
 
+                _socket.SendAsync(sendArgs);
+            };
 
-                _socket.ConnectAsync(connectArgs);
 
-            }
+            _socket.ConnectAsync(connectArgs);
+
+
         }
 
-    
+
 
         private void Disconnect()
         {
