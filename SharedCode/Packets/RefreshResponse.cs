ea66fac85f8176715c6cf23cb6327357d8412c9e
casdeGroot <casdegroot@live.nl>
2017-01-19 21:48:24 +0100

    drawing user + friends works

--- SharedCode/Packets/RefreshResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SharedCodePortable.Packets
-{
-    public class RefreshResponse
-    {
-        //TODO not sure if this should be response
-        // Idea is to refresh the friends list internal in the server before asking for this.
-        public User user;
-    }
-}
